5cb6f21ce27fa108570032eb7b0dda56068de38d
teamcity <teamcity@ciena.com>
2016-07-24 21:41:58 -0700

    adds integration test

---
+++ tests/integration-spec.js
@@ -0,0 +1,39 @@
+var exec = require('child-process-promise').exec
+var expect = require('chai').expect
+
+describe('integration test', function () {
+  beforeEach(function () {
+    console.log('before')
+  })
+  it('exists', function () {
+    return exec('bunsen')
+      .then((response) => {
+        expect(response).to.be.ok
+      })
+  })
+  it('it gives help', function () {
+    return exec('bunsen -h')
+      .then((response) => {
+        expect(response).to.be.ok
+      })
+  })
+  it('has a validate command', function () {
+    return exec('bunsen validate')
+      .then((response) => {
+        expect(response).to.be.ok
+      })
+      .catch((error) => {
+        expect(error).to.be.ok
+      })
+  })
+  it('has a convert command', function () {
+    return exec('bunsen convert')
+      .then((response) => {
+        expect(response).to.be.ok
+      })
+      .catch((error) => {
+        expect(error).to.be.ok
+      })
+  })
+})
+
