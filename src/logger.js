cfc2356e1467e4646a904c8d3c2d5b78412e6db8
Matthew Dahl <dahl.matthew@gmail.com>
2016-09-14 13:23:02 -0700

    Add some documentation

--- src/logger.js
+++ src/logger.js
@@ -1,36 +1,59 @@
 require('colors')
 
+const logLevelColors = {
+  error: 'red',
+  info: 'cyan',
+  success: 'green',
+  warn: 'yellow'
+}
+
 module.exports = function (verbose) {
+  /**
+   * Verbose flag
+   * @type {Boolean}
+   */
   this.verbose = verbose
+
+  /**
+   * Log message when verbose flag is enabled
+   * @param {String} msg - message to log
+   */
   this.log = (msg) => {
     if (this.verbose) {
       console.log(msg)
     }
   }
 
-  this.success = (msg) => {
-    console.log('\nSUCCESS: '.green + '\n' + msg)
-  }
-
-  this.info = (msg) => {
-    console.info('INFO: '.cyan + msg)
-  }
-
-  this.error = (msg) => {
-    if (msg instanceof Object) {
-      msg = JSON.stringify(msg, null, 2)
-    }
-    console.error('ERROR: '.red + msg)
-  }
-
-  this.warn = (msg) => {
-    if (msg instanceof Object) {
-      msg = JSON.stringify(msg, null, 2)
-    }
-    console.warn('WARNING: '.yellow + msg)
-  }
-
+  /**
+   * Print message
+   * @param {String} msg - message to print
+   */
   this.print = (msg) => {
     console.log(msg)
+  }
+
+  /**
+   * Generate methods for various log levels
+   */
+  for (var logLevel in logLevelColors) {
+    if (!logLevelColors.hasOwnProperty(logLevel)) {
+      continue
+    }
+
+    /**
+     * Log message at a given log level
+     * @param {String} msg - message to log
+     */
+    this[logLevel] = (msg) => {
+      const color = logLevelColors[logLevel]
+      const label = logLevel.toUpperCase()[color]
+      const sep = logLevel === 'success' ? '\n' : ' '
+
+      if (msg instanceof Object) {
+        msg = JSON.stringify(msg, null, 2)
+      }
+
+      console[logLevel](`${label}:${sep}${msg}`)
+    }
   }
 }
